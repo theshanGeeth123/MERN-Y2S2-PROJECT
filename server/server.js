551e932472e8d7a548400180ba553d5f7587bf9b
Theshan <theshangeethanjana@gmail.com>
2025-07-25 19:52:11 +0530

    Upto backend register has completed

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}));
+
+app.get("/",(req,res)=>res.send("API working"));
+
+app.listen(port,()=>console.log(`Server started on PORT:${port}
+`));
+
+//greatstack123
+
+//mongodb+srv://greatstack:<db_password>@cluster0.cui2so9.mongodb.net/
