b99a62ee847f70dd3da716efc5dd022a754f9d20
Theshan <theshangeethanjana@gmail.com>
2025-07-29 20:53:14 +0530

    Products backend part has been completed

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import adminRouter from './routes/Admin.route.js'
+import productRouter from './routes/Product.routes.js'
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get("/",(req,res)=>res.send("API working"));
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
 app.use('/api/admin',adminRouter);
+app.use('/api/product',productRouter);
 
 app.listen(port,()=>console.log(`Server started on PORT:${port}
 `));
