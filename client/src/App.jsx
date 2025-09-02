9fc17b5a5f7a930efd5da7159ce57967666f2d71
Malithi <malithipabodha08@gmail.com>
2025-09-02 19:08:37 +0530

    Added routing to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,10 @@ import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PackageCreate from './admin/PackageCreate';
+import Packages from './admin/Packages'; 
+import PackageDetail from './admin/PackageDetail'
+import UserPackages from './pages/UserPackages'
 
 // Admin section
 
@@ -26,12 +30,16 @@ function App() {
         <Route path='/reset-password' element={<ResetPassword/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/customer-home' element={<CustomerHome/>}/>
+        <Route path="/userpackages" element={<UserPackages />} />
         
 
         {/* admin */}
 
         <Route path='/admin/login' element={<AdminLogin/>}/>
         <Route path='/admin/home' element={<AdminHome/>}/>
+        <Route path="/packageCreate" element={<PackageCreate />} /> 
+        <Route path="/admin/packages" element={<Packages />} />
+        <Route path="/packageDetail/:id" element={<PackageDetail />} />
 
       </Routes>
     </div>
