c313e4d95e8f98730b4af983129f713096d2b258
dhushan7 <it23617414@my.sliit.lk>
2025-09-18 15:49:05 +0530

    Request backend is working

--- server/models/mRequest.model.js
+++ server/models/mRequest.model.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const requestSchema = new mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, lowercase: true, trim: true },
     amount: { type: Number, required: true, min: 0 },
